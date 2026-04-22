2841b41b624fceb8c1e2066cfc5ff61c9ae238a3
alvarocaastro <alvarocastro.gal@gmail.com>
2026-04-22 19:43:45 +0200

    [P9] Remove stage_checks dev scripts from repo

--- stage_checks/run_stage5_check.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from common import build_parser, run_stage_check
-
-
-if __name__ == "__main__":
-    args = build_parser(5).parse_args()
-    run_stage_check(5, clean=not args.no_clean, cache=args.cache)
